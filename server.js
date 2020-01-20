914a18b704fea03dafe6b623203a9d980d845102
Aljaz <mrakic.aljaz9@gmail.com>
2020-01-20 13:23:38 +0100

    displaying html page on request

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-var http = require('http');
+var http = require('http'); // http protocol
+var fs = require('fs'); // file system
+var dt = require('./date'); // refrencing a date script
 
-http.createServer(function (req, res){
-    res.writeHead(200, {'Content-Type':'text-plain'});
-    res.end("Hello World!");
-}).listen(8080);
+http.createServer(function (req, res){ // creates a server
+    fs.readFile('main_page.html', function(err, data){ // reads the main_page
+        if(err) throw err; // if there is an error
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data); // writes the data
+        res.end(); // end of response
+    });
+}).listen(8080); // listens on port 8080
