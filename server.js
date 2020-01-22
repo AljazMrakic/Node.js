5f67f29b9deb4dded67498787bd339802810f708
Aljaz <mrakic.aljaz9@gmail.com>
2020-01-22 22:02:46 +0100

    changes

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var http = require('http'); // http protocol
+var url = require('url'); // url module
 var fs = require('fs'); // file system
-var dt = require('./date'); // refrencing a date script
 
 http.createServer(function (req, res){ // creates a server
-    fs.readFile('main_page.html', function(err, data){ // reads the main_page
-        if(err) throw err; // if there is an error
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+
+    fs.readFile(filename, function(err, data){ // reads the main_page
+        if(err) { // if there is an error
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end(uc("404 Not Found")); // writes error 404
+        }
+
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data); // writes the data
         res.end(); // end of response
